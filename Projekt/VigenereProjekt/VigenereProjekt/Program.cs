057bbe6ae808157c3ae2d1c5520649473b1958bb
janesz315 <borosuveg6@gmail.com>
2024-01-17 20:45:20 +0100

    Apró módosítások

--- Projekt/VigenereProjekt/VigenereProjekt/Program.cs
+++ Projekt/VigenereProjekt/VigenereProjekt/Program.cs
@@ -1,19 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace VigenereProjekt
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//        }
-//    }
-//}
-using System;
+﻿using System;
 
 class Program
 {
