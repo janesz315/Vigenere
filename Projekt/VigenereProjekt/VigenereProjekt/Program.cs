89056fc32de0a142fbf3fe873e0ff86baf857d24
janesz315 <borosuveg6@gmail.com>
2024-01-30 22:30:48 +0100

    Egy kis tökéletesítés

--- Projekt/VigenereProjekt/VigenereProjekt/Program.cs
+++ Projekt/VigenereProjekt/VigenereProjekt/Program.cs
@@ -5,53 +5,41 @@ class Program
 {
     static void Main()
     {
+        //Változók deklarálása
         int i = 0, j = 0, k = 0, d = 0;
-
         char c;
-
+        //karaktertömbök készítése
         char[] kulcsE = new char[6]; // 5 karakter + a stringet lezáró karakter (\0)
-
         char[] szoveg = new char[256]; // 255 karakter + a stringet lezáró karakter (\0)
-
         char[] szovegKodolt = new char[256]; // 255 karakter + a stringet lezáró karakter (\0)
+        //Használatba szeretném venni a fájlokat.
         string szovege = "nyiltSzoveg.txt";
         string fajlKodolas = "kodolt.txt";
         string fajlDekodolas = "kodoltMegfejtés.txt";
-        
-
-
-        Console.WriteLine("[K]odolni vagy [D]ekodolni szeretnel? (K/D)");
+        //A kulcs és a módszer bekérése
+        Console.WriteLine("[K]ódolni vagy [D]ekódolni szeretnél? (K/D)");
         c = Console.ReadKey().KeyChar;
-        Console.WriteLine("\nKerem a kulcsot!");
+        Console.WriteLine("\nKérem a kulcsot!");
         kulcsE = Console.ReadLine().ToCharArray();
-
+        //A kulcs betûit nagybetûvé alakítom.
         for (int l = 0; l < kulcsE.Length; l++)
         {
             kulcsE[l] = char.ToUpper(kulcsE[l]);
         }
-        //Console.WriteLine("Kerem a nyilt szoveget!");
-        //szoveg = Console.ReadLine().ToCharArray();
-
+        
         int x = szoveg.Length;
 
-        // Ha az elsoCharArray rövidebb, ismételd meg annyiszor, hogy elérje a masodikCharArray hosszát
+        // Ha az elsõ karaktertömb rövidebb, ismételd meg annyiszor, hogy elérje a második karaktertömb hosszát
         while (kulcsE.Length < x)
         {
-            kulcsE = ConcatenateArrays(kulcsE,kulcsE );
+            kulcsE = ConcatenateArrays(kulcsE,kulcsE);
         }
 
-        // Vegyük csak az x hosszúságú részét az elsoCharArray-nek
+        // Vegyük csak az x hosszúságú részét az elsõ karaktertömbnek
         char[] kulcs = new char[x];
         Array.Copy(kulcsE, kulcs, x);
 
-        //for (k = 0; k < kulcs.Length && kulcs[k] != '\0'; k++)
-        //{
-        //    //Kisbetû->nagybetû konvertálás a kulcsban
-        //    if (kulcs[k] >= 'a' && kulcs[k] <= 'z')
-        //        kulcs[k] = (char)(kulcs[k] - 32);
-        //    d++;
-        //}
-
+        
         if (c == 'K' || c == 'k')
         {
         
@@ -69,18 +57,10 @@ class Program
                 if (j == d)
                     j = 0;
 
-                //if (szoveg[i] >= 'a' && szoveg[i] <= 'z')
-                //{
-                //    // A eset: a nyílt szöveg karaktere kisbetû
-                //    if (((szoveg[i] - 97) + (kulcs[j] - 65)) <= 25)
-                //        szoveg[i] = (char)(szoveg[i] + (kulcs[j] - 65));
-                //    else
-                //        szoveg[i] = (char)(szoveg[i] + (kulcs[j] - 65) - 26);
-                //}
-
+                
                 if (szoveg[i] >= 'A' && szoveg[i] <= 'Z')
                 {
-                    // B eset: a nyílt szöveg karaktere nagybetû
+                    // A nyílt szöveg karaktere nagybetû
                     if ((szoveg[i] - 65) + (kulcs[j] - 65) <= 25)
                         szoveg[i] = (char)(szoveg[i] + (kulcs[j] - 65));
                     else
@@ -95,7 +75,7 @@ class Program
                 // A string tömb minden elemét kiírjuk a fájlba
                 writer.Write(new string(szoveg));
             }
-            //Console.WriteLine("A kodolt szoveg: " + new string(szoveg));
+            
             Console.WriteLine("A fájl kódolva --> {0}", fajlKodolas);
 
         }
@@ -115,18 +95,10 @@ class Program
                 if (j == d)
                     j = 0;
 
-                //if (szoveg[i] >= 'a' && szoveg[i] <= 'z')
-                //{
-                //    // A eset: a dekódolandó szöveg karaktere kisbetû
-                //    if ((szoveg[i] - 97) - (kulcs[j] - 65) >= 0)
-                //        szoveg[i] = (char)(szoveg[i] - (kulcs[j] - 65));
-                //    else
-                //        szoveg[i] = (char)(szoveg[i] - (kulcs[j] - 65) + 26);
-                //}
 
                 if (szoveg[i] >= 'A' && szoveg[i] <= 'Z')
                 {
-                    // B eset: a dekódolandó szöveg karaktere nagybetû
+                    //A dekódolandó szöveg karaktere nagybetû
                     if ((szoveg[i] - 65) - (kulcs[j] - 65) >= 0)
                         szoveg[i] = (char)(szoveg[i] - (kulcs[j] - 65));
                     else
@@ -141,7 +113,7 @@ class Program
                 // A string tömb minden elemét kiírjuk a fájlba
                 writer.Write(new string(szoveg));
             }
-            //Console.WriteLine("A dekodolt szoveg: " + new string(szoveg));
+            
             Console.WriteLine("A fájl dekódolva --> {0}", fajlDekodolas);
 
         }
@@ -151,6 +123,7 @@ class Program
 
     static char[] ConcatenateArrays(char[] array1, char[] array2)
     {
+        //itt adom össze a karaktertömböket annyiszor, amennyiszer meghívom azt.
         char[] result = new char[array1.Length + array2.Length];
         Array.Copy(array1, result, array1.Length);
         Array.Copy(array2, 0, result, array1.Length, array2.Length);
